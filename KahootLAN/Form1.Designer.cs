414b7005c3c0674ecd1a80761d71df14898e7d46
SLOVAKIA-ENTITI <mpaskala08@gmail.com>
2025-06-03 16:48:17 +0200

    Panel 4 design

--- KahootLAN/Form1.Designer.cs
+++ KahootLAN/Form1.Designer.cs
@@ -61,6 +61,11 @@ namespace KahootLAN
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -83,6 +88,7 @@ namespace KahootLAN
             // 
             // btnJoin
             // 
+            this.btnJoin.BackColor = System.Drawing.Color.White;
             this.btnJoin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.btnJoin.Location = new System.Drawing.Point(279, 212);
             this.btnJoin.Margin = new System.Windows.Forms.Padding(2);
@@ -90,11 +96,12 @@ namespace KahootLAN
             this.btnJoin.Size = new System.Drawing.Size(56, 30);
             this.btnJoin.TabIndex = 1;
             this.btnJoin.Text = "Join";
-            this.btnJoin.UseVisualStyleBackColor = true;
+            this.btnJoin.UseVisualStyleBackColor = false;
             this.btnJoin.Click += new System.EventHandler(this.btnJoin_Click_1);
             // 
             // btnStartQuiz
             // 
+            this.btnStartQuiz.BackColor = System.Drawing.Color.White;
             this.btnStartQuiz.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.btnStartQuiz.Location = new System.Drawing.Point(255, 251);
             this.btnStartQuiz.Margin = new System.Windows.Forms.Padding(2);
@@ -103,7 +110,7 @@ namespace KahootLAN
             this.btnStartQuiz.TabIndex = 2;
             this.btnStartQuiz.Text = "Start Quiz";
             this.btnStartQuiz.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btnStartQuiz.UseVisualStyleBackColor = true;
+            this.btnStartQuiz.UseVisualStyleBackColor = false;
             this.btnStartQuiz.Visible = false;
             this.btnStartQuiz.Click += new System.EventHandler(this.btnStartQuiz_Click_1);
             // 
@@ -147,6 +154,7 @@ namespace KahootLAN
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.White;
             this.button1.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.button1.Location = new System.Drawing.Point(159, 296);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
@@ -154,7 +162,7 @@ namespace KahootLAN
             this.button1.Size = new System.Drawing.Size(90, 24);
             this.button1.TabIndex = 7;
             this.button1.Text = "Make test";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -185,6 +193,7 @@ namespace KahootLAN
             // 
             // btnLoadQuestions
             // 
+            this.btnLoadQuestions.BackColor = System.Drawing.Color.White;
             this.btnLoadQuestions.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.btnLoadQuestions.Location = new System.Drawing.Point(370, 296);
             this.btnLoadQuestions.Margin = new System.Windows.Forms.Padding(2);
@@ -192,7 +201,7 @@ namespace KahootLAN
             this.btnLoadQuestions.Size = new System.Drawing.Size(90, 24);
             this.btnLoadQuestions.TabIndex = 6;
             this.btnLoadQuestions.Text = "Load Test";
-            this.btnLoadQuestions.UseVisualStyleBackColor = true;
+            this.btnLoadQuestions.UseVisualStyleBackColor = false;
             this.btnLoadQuestions.Visible = false;
             this.btnLoadQuestions.Click += new System.EventHandler(this.btnLoadQuestions_Click);
             // 
@@ -290,6 +299,7 @@ namespace KahootLAN
             // 
             // btnSubmit
             // 
+            this.btnSubmit.BackColor = System.Drawing.Color.White;
             this.btnSubmit.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.btnSubmit.Location = new System.Drawing.Point(255, 268);
             this.btnSubmit.Margin = new System.Windows.Forms.Padding(2);
@@ -303,6 +313,7 @@ namespace KahootLAN
             // 
             // btnNextQuestion
             // 
+            this.btnNextQuestion.BackColor = System.Drawing.Color.White;
             this.btnNextQuestion.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.btnNextQuestion.Location = new System.Drawing.Point(207, 268);
             this.btnNextQuestion.Margin = new System.Windows.Forms.Padding(2);
@@ -310,13 +321,18 @@ namespace KahootLAN
             this.btnNextQuestion.Size = new System.Drawing.Size(205, 34);
             this.btnNextQuestion.TabIndex = 3;
             this.btnNextQuestion.Text = "Next Question";
-            this.btnNextQuestion.UseVisualStyleBackColor = true;
+            this.btnNextQuestion.UseVisualStyleBackColor = false;
             this.btnNextQuestion.Visible = false;
             this.btnNextQuestion.Click += new System.EventHandler(this.btnNextQuestion_Click);
             // 
             // panel4
             // 
             this.panel4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel4.BackgroundImage")));
+            this.panel4.Controls.Add(this.label8);
+            this.panel4.Controls.Add(this.label7);
+            this.panel4.Controls.Add(this.label6);
+            this.panel4.Controls.Add(this.label5);
+            this.panel4.Controls.Add(this.label4);
             this.panel4.Controls.Add(this.comboBox1);
             this.panel4.Controls.Add(this.button5);
             this.panel4.Controls.Add(this.button4);
@@ -337,109 +353,173 @@ namespace KahootLAN
             // 
             // comboBox1
             // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "2",
             "3",
             "4"});
-            this.comboBox1.Location = new System.Drawing.Point(345, 56);
+            this.comboBox1.Location = new System.Drawing.Point(360, 130);
             this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(29, 21);
+            this.comboBox1.Size = new System.Drawing.Size(30, 21);
             this.comboBox1.TabIndex = 9;
-            this.comboBox1.Visible = false;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // button5
             // 
+            this.button5.BackColor = System.Drawing.Color.White;
             this.button5.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button5.Location = new System.Drawing.Point(241, 268);
+            this.button5.Location = new System.Drawing.Point(486, 307);
             this.button5.Margin = new System.Windows.Forms.Padding(2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(74, 33);
+            this.button5.Size = new System.Drawing.Size(103, 33);
             this.button5.TabIndex = 8;
-            this.button5.Text = "vytvorit";
-            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Text = "VYTVORIŤ";
+            this.button5.UseVisualStyleBackColor = false;
             // 
             // button4
             // 
+            this.button4.BackColor = System.Drawing.Color.White;
             this.button4.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button4.Location = new System.Drawing.Point(83, 268);
+            this.button4.Location = new System.Drawing.Point(411, 306);
             this.button4.Margin = new System.Windows.Forms.Padding(2);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(71, 34);
             this.button4.TabIndex = 7;
-            this.button4.Text = "pridat";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Text = "PRIDAŤ";
+            this.button4.UseVisualStyleBackColor = false;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(241, 142);
+            this.textBox5.Location = new System.Drawing.Point(498, 222);
             this.textBox5.Margin = new System.Windows.Forms.Padding(2);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(68, 20);
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
             this.textBox5.TabIndex = 6;
             this.textBox5.Visible = false;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(97, 142);
+            this.textBox4.Location = new System.Drawing.Point(389, 222);
             this.textBox4.Margin = new System.Windows.Forms.Padding(2);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(68, 20);
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
             this.textBox4.TabIndex = 5;
             this.textBox4.Visible = false;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(97, 203);
+            this.textBox3.Location = new System.Drawing.Point(279, 222);
             this.textBox3.Margin = new System.Windows.Forms.Padding(2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(68, 20);
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 4;
             this.textBox3.Visible = false;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(97, 174);
+            this.textBox2.Location = new System.Drawing.Point(279, 260);
             this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(68, 20);
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 3;
             this.textBox2.Visible = false;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(170, 107);
+            this.textBox1.Location = new System.Drawing.Point(196, 178);
             this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(79, 20);
+            this.textBox1.Size = new System.Drawing.Size(364, 20);
             this.textBox1.TabIndex = 2;
             // 
             // button3
             // 
+            this.button3.BackColor = System.Drawing.Color.White;
             this.button3.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button3.Location = new System.Drawing.Point(255, 57);
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(279, 129);
             this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(80, 33);
             this.button3.TabIndex = 1;
-            this.button3.Text = "viac otazok";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Text = "MULTI";
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.White;
             this.button2.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button2.Location = new System.Drawing.Point(72, 57);
+            this.button2.Location = new System.Drawing.Point(193, 130);
             this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(82, 33);
             this.button2.TabIndex = 0;
-            this.button2.Text = "ano/nie";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Text = "ÁNO/NIE";
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Arial Black", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(0, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(600, 74);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "TVORBA OTÁZOK";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(25, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(163, 31);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Typ otázky";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(28, 171);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(160, 27);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Znenie otázky";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(28, 215);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(233, 27);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Nesprávne odpovede";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(28, 257);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(196, 27);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Správna odpoveď";
             // 
             // Form1
             // 
@@ -447,9 +527,9 @@ namespace KahootLAN
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel3);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -497,6 +577,11 @@ namespace KahootLAN
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
     }
 }
 
